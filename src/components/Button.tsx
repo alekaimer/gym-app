e6587adac8fac66aad6ac8d5d5d39b95986a4c26
Alexandre Kaimer <alekaimer@gmail.com>
2022-11-06 16:45:28 -0300

    feat: adds Button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,34 @@
+import { Button as ButtonNative, Text, IButtonProps } from "native-base";
+
+interface ButtonProps extends IButtonProps {
+  title: string;
+  type?: "primary" | "secondary";
+}
+
+export function Button({ title, type = "primary", ...rest }: ButtonProps) {
+  return (
+    <ButtonNative
+      w="full"
+      h={14}
+      rounded="sm"
+      fontSize="md"
+      bg={type === "secondary" ? "red.500" : "yellow.500"}
+      _pressed={{ bg: type === "secondary" ? "red.600" : "yellow.600" }}
+      _loading={{
+        _spinner: {
+          color: "black",
+        },
+      }}
+      {...rest}
+    >
+      <Text
+        textTransform="uppercase"
+        fontFamily={"heading"}
+        fontSize={"sm"}
+        color={type === "secondary" ? "white" : "black"}
+      >
+        {title}
+      </Text>
+    </ButtonNative>
+  );
+}
