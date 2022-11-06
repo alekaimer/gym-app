c3e5b1705741b6bfdba1f9ca81cba3db8bc07d53
Alexandre Kaimer <alekaimer@gmail.com>
2022-11-06 16:47:06 -0300

    refactor: SignIn screen

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,14 +1,28 @@
-import { Center, Text } from "native-base";
+import { Center, Text, Icon } from "native-base";
 import React from "react";
 
-import Logo from '../assets/logo.svg'
+import Logo from "../assets/logo.svg";
+import { Button } from "../components/Button";
+import { Fontisto } from "@expo/vector-icons";
 
 function SignIn() {
   return (
-    <Center flex={1} bgColor="gray.900">
+    <Center flex={1} bgColor="gray.900" p={7}>
       <Logo width={212} height={40} />
-      <Text color="yellow.500" fontSize={24}>
-        SignIn
+
+      <Button
+        title={"Entrar"}
+        leftIcon={
+          <Icon as={Fontisto} name={"google"} color={"white"} size={"sm"} />
+        }
+        type="secondary"
+        mt={20}
+        mb={5}
+      />
+
+      <Text color={"white"} textAlign={"center"}>
+        Não utilizamos nenhuma informação além{"\n"}
+        do seu e-mail para criação de sua conta.
       </Text>
     </Center>
   );
