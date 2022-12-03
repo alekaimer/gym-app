ed82033f18f41170dac3a9bd9657690aac66a758
Alexandre Kaimer <alekaimer@gmail.com>
2022-12-02 22:27:16 -0300

    feat: adds Button component with two variants

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,30 +3,32 @@ import { Button as ButtonNative, Text, IButtonProps } from "native-base";
 
 interface ButtonProps extends IButtonProps {
   title: string;
-  type?: "primary" | "secondary";
+  variant?: "solid" | "outline";
 }
 
-export function Button({ title, type = "primary", ...rest }: ButtonProps) {
+export function Button({ title, variant, ...props }: ButtonProps) {
   return (
     <ButtonNative
       w="full"
       h={14}
       rounded="sm"
       fontSize="md"
-      bg={type === "secondary" ? "red.500" : "yellow.500"}
-      _pressed={{ bg: type === "secondary" ? "red.600" : "yellow.600" }}
+      borderColor="green.500"
+      bg={variant === "outline" ? "transparent" : "green.700"}
+      borderWidth={variant === "outline" ? 1 : 0}
+      _pressed={{ bg: variant === "outline" ? "gray.500" : "green.500" }}
       _loading={{
         _spinner: {
           color: "black",
         },
       }}
-      {...rest}
+      {...props}
     >
       <Text
         textTransform="uppercase"
-        fontFamily={"heading"}
-        fontSize={"sm"}
-        color={type === "secondary" ? "white" : "black"}
+        fontFamily="heading"
+        fontSize="sm"
+        color={variant === "outline" ? "green.500" : "white"}
       >
         {title}
       </Text>
