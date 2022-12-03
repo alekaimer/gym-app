03916c85290a50fd97fa72a615b6fdf9b78ce7c4
Alexandre Kaimer <alekaimer@gmail.com>
2022-12-03 18:02:53 -0300

    feat: adds Hometemplate

---
+++ src/templates/HomeTemplate.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Box } from "native-base";
+import { HomeHeader } from "@components/HomeHeader";
+
+function HomeTemplate({ children }: { children: React.ReactNode }) {
+
+  return (
+    <Box flex="1" p={0}>
+      <HomeHeader safeAreaTop />
+      <Box flex="1" p={0}>
+        {children}
+      </Box>
+    </Box>
+  );
+}
+
+export default HomeTemplate;
