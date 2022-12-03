ff72374f4cfb8260f91bb3f383002a0d1147679d
Alexandre Kaimer <alekaimer@gmail.com>
2022-12-02 22:35:22 -0300

    feat: adds statusbar

--- App.tsx
+++ App.tsx
@@ -11,16 +11,21 @@ import { NativeBaseProvider } from "native-base";
 import Loading from "@components/Loading";
 import { AuthContextProvider } from "@contexts/AuthContext";
 import { MainRoutes } from "@routes/index";
+import { StatusBar } from "react-native";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     Roboto_400Regular,
-    Roboto_500Medium,
     Roboto_700Bold,
   });
 
   return (
     <NativeBaseProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
       <AuthContextProvider>
         {!fontsLoaded ? <Loading /> : <MainRoutes />}
       </AuthContextProvider>
