3abaa0c178973855923167d39c3805b35815446c
Alexandre Kaimer <alekaimer@gmail.com>
2022-12-03 13:41:04 -0300

    refactor: adds HomeHeader in template

--- src/templates/WithHeaderTemplate.tsx
+++ src/templates/WithHeaderTemplate.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 import { Box } from "native-base";
 import Header from "@components/Header";
+import { HomeHeader } from "@components/HomeHeader";
 
 function WithHeaderTemplate({ children }: { children: React.ReactNode }) {
 
   return (
-    <Box flex="1" bgColor="gray.400" p={0} safeAreaTop>
-      <Header />
+    <Box flex="1" bgColor="gray.400" p={0}>
+      <HomeHeader safeAreaTop />
+      {/* <Header />
       <Box flex="1" bgColor="white" p={0}>
         {children}
-      </Box>
+      </Box> */}
     </Box>
   );
 }
