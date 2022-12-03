a612c31b7f0d8d9ec904c458bd0044e11773a5f0
Alexandre Kaimer <alekaimer@gmail.com>
2022-12-03 19:27:43 -0300

    refactor: list exercises in Hystory screen

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,14 +1,47 @@
 import React from "react";
-import { Center, Heading, ScrollView, VStack, useTheme } from "native-base";
+import { Heading, VStack, SectionList, Text } from "native-base";
 import { WithHeaderTemplate } from "@templates/WithHeaderTemplate";
+import { HistoryCard } from "@components/HistoryCard";
 
 export function History() {
-  const { colors } = useTheme();
+  const [exercises, setExercises] = React.useState([
+    // { title: "03.12.22", data: ["Puxada frontal", "Remada unilateral"] },
+    // { title: "04.12.22", data: ["Puxada frontal"] },
+    // { title: "03.12.22", data: ["Puxada frontal", "Remada unilateral"] },
+    // { title: "04.12.22", data: ["Puxada frontal"] },
+    // { title: "03.12.22", data: ["Puxada frontal", "Remada unilateral"] },
+    // { title: "04.12.22", data: ["Puxada frontal"] },
+    // { title: "03.12.22", data: ["Puxada frontal", "Remada unilateral"] },
+    // { title: "04.12.22", data: ["Puxada frontal"] },
+  ]);
+
   return (
-    <WithHeaderTemplate>
-      <Center flex={1}>
-        <Heading color="gray.100">History Screen</Heading>
-      </Center>
+    <WithHeaderTemplate title="Histórico de Exercícios">
+      <VStack flex={1} px={8} py={0}>
+        <SectionList
+          sections={exercises}
+          keyExtractor={(item, index) => item + index}
+          renderItem={({ item }) => <HistoryCard />}
+          renderSectionHeader={({ section: { title } }) => (
+            <Heading color="gray.200" fontSize="md" mb={3} mt={10}>
+              {title}
+            </Heading>
+          )}
+          contentContainerStyle={
+            exercises.length === 0 && {
+              flex: 1,
+              justifyContent: "center",
+              alignItems: "center",
+            }
+          }
+          ListEmptyComponent={
+            <Text color="gray.200" fontSize="md" textAlign="center">
+              Não há exercícios registrados.{"\n"}
+              Vamos começar hoje?
+            </Text>
+          }
+        />
+      </VStack>
     </WithHeaderTemplate>
   );
 }
