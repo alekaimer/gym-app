0305ec185b18a914ab00820010b09d5555eed5c6
Alexandre Kaimer <alekaimer@gmail.com>
2022-12-06 20:37:23 -0300

    feat: add image picker and change photo action

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {
   Center,
   Heading,
@@ -9,13 +9,38 @@ import {
 } from "native-base";
 import { WithHeaderTemplate } from "@templates/WithHeaderTemplate";
 import { UserPhoto } from "@components/UserPhoto";
-import { TouchableOpacity } from "react-native";
+import { Alert, Platform, TouchableOpacity } from "react-native";
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
+import * as ImagePicker from "expo-image-picker";
 
 const PHOTO_SIZE = 33;
 
 export function Profile() {
+  const [userPhoto, setUserPhoto] = React.useState<string | null>(null);
+
+  async function handleUserPhotoSelect() {
+    await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      quality: 1,
+      aspect: [4, 4],
+      allowsEditing: true,
+    });
+
+    if (result.cancelled) {
+      return;
+    }
+
+    setUserPhoto(result.uri);
+    return result;
+  }
+
+  useEffect(() => {
+    // handleUserPhotoSelect();
+  }, []);
+
   return (
     <WithHeaderTemplate title="Perfil">
       <ScrollView>
@@ -30,7 +55,7 @@ export function Profile() {
           >
             <UserPhoto
               source={{
-                uri: "https://github.com/alekaimer.png",
+                uri: userPhoto || "https://github.com/alekaimer.png",
               }}
               alt="Imagem do usuário"
               size={PHOTO_SIZE}
@@ -38,7 +63,13 @@ export function Profile() {
           </Skeleton>
 
           <TouchableOpacity>
-            <Text color="green.500" fontWeight="bold" mt={2} mb={8}>
+            <Text
+              color="green.500"
+              fontWeight="bold"
+              mt={2}
+              mb={8}
+              onPress={handleUserPhotoSelect}
+            >
               Alterar foto
             </Text>
           </TouchableOpacity>
@@ -57,6 +88,7 @@ export function Profile() {
             isDisabled
           />
         </Center>
+
         <VStack px={10} mt={6} mb={9}>
           <Heading mt={8} mb={4} size="sm" color="gray.200">
             Alterar senha
