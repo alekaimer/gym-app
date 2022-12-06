3a2e2fee03acb7177717977918933a63d3eb9261
Alexandre Kaimer <alekaimer@gmail.com>
2022-12-06 18:43:14 -0300

    fix: insert border in Input component

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -4,14 +4,15 @@ import { Input as InputNativeBase, IInputProps } from "native-base";
 export function Input({ ...props }: IInputProps) {
   return (
     <InputNativeBase
-      bg="gray.700"
       h={14}
       px={4}
-      borderWidth={0}
       fontSize="md"
       color="white"
       fontFamily="body"
       placeholderTextColor="gray.300"
+      borderWidth={1}
+      borderColor="gray.700"
+      bg="gray.700"
       _focus={{
         borderWidth: 1,
         borderColor: "green.500",
