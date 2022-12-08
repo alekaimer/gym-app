ff5bfa2d28d1f2313fc5efb0b2d74648b64d9cb9
Alexandre Kaimer <alekaimer@gmail.com>
2022-12-08 18:11:16 -0300

    refactor: adds MainRoutes in ReactNavigation global types

--- src/@types/navigation.d.ts
+++ src/@types/navigation.d.ts
@@ -1,11 +1,12 @@
 import { MainRoutesParams } from "@routes/index";
 import { AuthRouterParams } from "@routes/auth.routes";
-import { HomeRoutesParams } from "../routes/home.routes";
+import { HomeRoutesParams } from "@routes/home.routes";
 
 declare global {
   namespace ReactNavigation {
     interface RootParamList
       extends MainRoutesParams,
+        MainRoutesParams,
         HomeRoutesParams,
         AuthRouterParams {}
   }
