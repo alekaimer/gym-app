d397e010450e1142ddbe48ed0d227ac579b068b5
Alexandre Kaimer <alekaimer@gmail.com>
2022-12-17 00:37:08 -0300

    feat: adds fetch exercise by group

--- src/components/ExerciseCard.tsx
+++ src/components/ExerciseCard.tsx
@@ -3,9 +3,14 @@ import { TouchableOpacity, TouchableOpacityProps } from "react-native";
 import { Heading, HStack, Icon, Image, Text, VStack } from "native-base";
 import { Entypo } from "@expo/vector-icons";
 
-type ExerciseCardProps = TouchableOpacityProps & {};
+import { ExercisesDTO } from "@dtos/ExercisesDTO";
+import { api } from "@services/api";
 
-export const ExerciseCard = ({ ...rest }: ExerciseCardProps) => {
+type ExerciseCardProps = TouchableOpacityProps & {
+  data: ExercisesDTO;
+};
+
+export const ExerciseCard = ({ data, ...rest }: ExerciseCardProps) => {
   return (
     <TouchableOpacity {...rest}>
       <HStack
@@ -18,7 +23,7 @@ export const ExerciseCard = ({ ...rest }: ExerciseCardProps) => {
       >
         <Image
           source={{
-            uri: "http://conteudo.imguol.com.br/c/entretenimento/0c/2019/12/03/remada-unilateral-com-halteres-1575402100538_v2_600x600.jpg",
+            uri: `${api.defaults.baseURL}/exercise/thumb/${data.thumb}`,
           }}
           alt="Imagem do exercício"
           w={16}
@@ -29,11 +34,11 @@ export const ExerciseCard = ({ ...rest }: ExerciseCardProps) => {
         />
         <VStack flex={1}>
           <Heading color="white" fontSize="lg">
-            Puzada frontal
+            {data.name}
           </Heading>
 
           <Text color="gray.200" fontSize="sm" numberOfLines={2}>
-            3 séries x 12 repetições
+            {data.series} séries x {data.repetitions} repetições
           </Text>
         </VStack>
 
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,6 +1,6 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { FlatList, Heading, HStack, Text, useToast, VStack } from "native-base";
-import { useNavigation } from "@react-navigation/native";
+import { useFocusEffect, useNavigation } from "@react-navigation/native";
 
 import { api } from "@services/api";
 import { AppError } from "@utils/AppError";
@@ -8,10 +8,13 @@ import { AppError } from "@utils/AppError";
 import HomeTemplate from "@templates/HomeTemplate";
 import { Group } from "@components/Group";
 import { ExerciseCard } from "@components/ExerciseCard";
+import { ExercisesDTO } from "@dtos/ExercisesDTO";
+import Loading from "@components/Loading";
 
 function Home() {
+  const [isLoading, setIsLoading] = useState(true);
   const [groups, setGroups] = useState<string[]>([]);
-  const [exercises, setExercises] = useState([1, 2, 3, 4, 5, 6, 7, 8]);
+  const [exercises, setExercises] = useState<ExercisesDTO[]>([]);
   const [activeGroup, setActiveGroup] = useState("costas");
 
   const { navigate } = useNavigation();
@@ -23,22 +26,55 @@ function Home() {
 
   async function fetchGroups() {
     try {
+      setIsLoading(true);
+
       const { data } = await api.get("/groups");
       setGroups(data);
     } catch (error) {
-      const isAppError = error instanceof AppError
-      const title = isAppError ? error.message : "Não foi possível carregar os grupos";
+      const isAppError = error instanceof AppError;
+      const title = isAppError
+        ? error.message
+        : "Não foi possível carregar os grupos";
       toast.show({
         title,
         placement: "top",
         bgColor: "red.500",
       });
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  async function fetchExercisesByGroup() {
+    try {
+      setIsLoading(true);
+
+      const { data } = await api.get(`/exercises/bygroup/${activeGroup}`);
+      setExercises(data);
+    } catch (error) {
+      const isAppError = error instanceof AppError;
+      const title = isAppError
+        ? error.message
+        : "Não foi possível carregar os exercícios";
+      toast.show({
+        title,
+        placement: "top",
+        bgColor: "red.500",
+      });
+    } finally {
+      setIsLoading(false);
     }
   }
 
   useEffect(() => {
     fetchGroups();
   }, []);
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchExercisesByGroup();
+    }, [activeGroup])
+  );
 
   return (
     <HomeTemplate>
@@ -48,7 +84,10 @@ function Home() {
           <Group
             name={item}
             isActive={activeGroup.toLowerCase() === item.toLowerCase()}
-            onPress={() => setActiveGroup(item)}
+            onPress={() => {
+              setActiveGroup(item);
+              fetchExercisesByGroup();
+            }}
           />
         )}
         horizontal
@@ -61,26 +100,32 @@ function Home() {
         minH={10}
       />
 
-      <VStack flex={1} px={`8`}>
-        <HStack justifyContent="space-between" mb={5}>
-          <Heading color="gray.200" fontSize="md">
-            Exercícios
-          </Heading>
-          <Text color="gray.200" fontSize="sm">
-            {exercises.length}
-          </Text>
-        </HStack>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <VStack flex={1} px={`8`}>
+          <HStack justifyContent="space-between" mb={5}>
+            <Heading color="gray.200" fontSize="md">
+              Exercícios
+            </Heading>
+            <Text color="gray.200" fontSize="sm">
+              {exercises.length}
+            </Text>
+          </HStack>
 
-        <FlatList
-          data={exercises}
-          keyExtractor={(item) => item.toString()}
-          renderItem={({ item }) => <ExerciseCard onPress={handleOpenExerciseDetails} />}
-          showsVerticalScrollIndicator={false}
-          _contentContainerStyle={{
-            pb: 20,
-          }}
-        />
-      </VStack>
+          <FlatList
+            data={exercises}
+            keyExtractor={(item) => item.id.toString()}
+            renderItem={({ item }) => (
+              <ExerciseCard data={item} onPress={handleOpenExerciseDetails} />
+            )}
+            showsVerticalScrollIndicator={false}
+            _contentContainerStyle={{
+              pb: 20,
+            }}
+          />
+        </VStack>
+      )}
     </HomeTemplate>
   );
 }
