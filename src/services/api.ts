428a62f3ee39a7d3311f97733921378623c29c5b
Alexandre Kaimer <alekaimer@gmail.com>
2022-12-31 13:10:49 -0300

    feat(refreshtoken): get old token

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,12 +1,13 @@
 import axios, { AxiosInstance } from "axios";
 
 import { AppError } from "@utils/AppError";
+import { storageAuthTokenGet } from "@storage/storageAuthToken";
 
 type SignOutProps = () => void;
 
 type APIInstanceProps = AxiosInstance & {
   registerInterceptTokenManager: (signOut: SignOutProps) => () => void;
-}
+};
 
 const api = axios.create({
   // baseURL: "http://172.21.112.1:3333",
@@ -19,11 +20,30 @@ api.registerInterceptTokenManager = (signOut) => {
     (response) => {
       return response;
     },
-    (error) => {
-      if(error.response && error.response.data) {
-        return Promise.reject(new AppError(error.response.data.message));
+    async (requestError) => {
+      // Verifica se o erro é de token inválido ou expirado
+      if (requestError?.response?.status === 401) {
+        // 401 = Unauthorized
+        if (
+          requestError.response.data.message === "token.expired" ||
+          requestError.response.data.message === "token.invalid"
+        ) {
+          const oldToken = await storageAuthTokenGet();
+
+          if (!oldToken) {
+            signOut();
+            return Promise.reject(new AppError(requestError));
+          }
+        }
+
+        signOut();
+      }
+
+      // Verifica e trata a exceção
+      if (requestError.response && requestError.response.data) {
+        return Promise.reject(new AppError(requestError.response.data.message));
       } else {
-        return Promise.reject(error);
+        return Promise.reject(requestError);
       }
     }
   );
@@ -31,7 +51,7 @@ api.registerInterceptTokenManager = (signOut) => {
   // Retorna uma função para desfazer o intercept
   return () => {
     api.interceptors.response.eject(interceptTokenManager);
-  }
-}
+  };
+};
 
 export { api };
